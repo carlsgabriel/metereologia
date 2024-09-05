e758fcbb56670ac77d122298bf37adf75b8b81d2
carlsgabriel <gomescarlosleite@gmail.com>
2024-09-05 15:59:43 -0300

    adding elements to html, styling them and creating background logic in script.js

--- script.js
+++ script.js
@@ -9,16 +9,21 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(data)}&appid=a3ad8700c8a4563718f6b9da0513e365&units=metric&lang=pt_br`;
         let req = await fetch(url);
         let json = await req.json();
-        
-        if(json.cod === 200){
+        console.log(json);
+
+        if (json.cod === 200) {
+            mensagem('');
             mostrarInfos(json);
+            console.log(json.weather[0].description);
         } else {
             limparInfos();
             mensagem('A cidade inserida está incorreta ou não existe.');
+            document.querySelector('body').style.backgroundImage = 'none';
         }
 
     } else {
         limparInfos();
+        document.querySelector('body').style.backgroundImage = 'none';
     }
 })
 
@@ -26,16 +31,46 @@ function mensagem(msg) {
     document.querySelector('.aviso').innerHTML = msg;
 }
 
-function limparInfos(){
+function limparInfos() {
     mensagem('');
     document.querySelector('.resultado').style.display = 'none';
 }
 
-function mostrarInfos(json){
+function mostrarInfos(json) {
     document.querySelector('.titulo').innerHTML = `<div class="titulo">${json.name}, ${json.sys.country}</div>`;
     document.querySelector('.tempInfo').innerHTML = `<div class="tempInfo">${json.main.temp} <sup>ºC</sup></div>`;
+    document.querySelector('.iconLegend').innerHTML = `<div class="iconLegend">${json.weather[0].description}</div>`;
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`);
     document.querySelector('.ventoInfo').innerHTML = `<div class="ventoInfo">${json.wind.speed} <span>km/h</span></div>`;
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.wind.deg - 90}deg)`;
+
+    document.querySelector('body').style.backgroundRepeat = 'no-repeat';
+    document.querySelector('body').style.backgroundSize = 'cover';
+    if (json.weather[0].description == 'céu limpo') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/ceu-limpo.jpg')`;
+    } else if (json.weather[0].description == 'chuva') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/chuva.jpeg')`;
+    } else if (json.weather[0].description == 'chuva leve') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/chuva-leve.jpg')`;
+    } else if (json.weather[0].description == 'chuvisco') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/chuvisco.jpg')`;
+    } else if (json.weather[0].description == 'neblina') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/neblina.webp')`;
+    } else if (json.weather[0].description == 'neve') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/neve.jpeg')`;
+    } else if (json.weather[0].description == 'nevoa') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/nevoa.jpg')`;
+    } else if (json.weather[0].description == 'nuvens dispersas') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/nuvens-esparsas.jpg')`;
+    } else if (json.weather[0].description == 'nuvens fragmentadas') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/nuvens-fragmentadas.jpg')`;
+    } else if (json.weather[0].description == 'algumas nuvens') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/poucas-nuvens.jpeg')`;
+    } else if (json.weather[0].description == 'nublado') {
+        document.querySelector('body').style.backgroundImage = `url('imgs/nublado.jpg')`;
+    } else {
+        document.querySelector('body').style.backgroundImage = `url('imgs/tempestade.webp')`;
+    }
+
     document.querySelector('.resultado').style.display = 'block';
 }
